8a1dee5333ad52602c0639c90936f6536f789c0e
Hor SengHab <horsenghab22@gmail.com>
2025-09-08 09:19:03 +0700

    Add Testimonial Page

--- assets/js/login.js
+++
@@ -1,9 +0,0 @@
-// Check login status
-const isLoggedIn = localStorage.getItem("isLoggedIn");
-
-if (isLoggedIn === "true") {
-  const btnContainer = document.querySelector(".d-flex.ms-3");
-  if (btnContainer) {
-    btnContainer.remove();
-  }
-}
